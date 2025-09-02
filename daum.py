4f6c8ae39545dea39cd0217dd214b2a80e62830f
tmznwnel00 <tmznwnel00@gmail.com>
2025-09-02 11:25:48 +0900

    feat: daum update

--- daum.py
+++ daum.py
@@ -3,19 +3,16 @@ import requests
 
 import time
 from selenium import webdriver
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 
 class Crawl():
     def __init__(self) -> None:
         #검색어 리스트
-
-        self.search = ['리튬', '사재기', '사우디', '탈모', '세계최초',
-                        '단독', 'FDA', '완전관해', '사멸', '암',
-                        '매각', '인수', '독점', '상용화',
-                        '경영권', '판호']
+        self.search = ['승인']
         # self.urls = [f"https://search.naver.com/search.naver?where=news&query={keyword}&sort=1&start=0" for keyword in self.search]
         self.urls = [f"https://search.daum.net/search?w=news&q={keyword}&sort=recency" for keyword in self.search]
+        # self.urls.extend(self.daum)
         self.list_set = set()
 
     def run(self, url):
@@ -32,26 +29,34 @@ class Crawl():
                 # articles = soup.select('.news_tit')
                 
                 #requests
-                req = requests.get(url)
-                soup = BeautifulSoup(req.text, 'html.parser')
-                articles = soup.select('.tit_main')
-                # print(daum)
-                for article in articles:
-                    title = article.text
-                    link = article['href']
-                    if link not in self.list_set:
-                        self.list_set.add(link)
-                        print(title, link)
-                    else:
-                        break
-                time.sleep(30)
+                header = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.46'}
+                req = requests.get(url, headers = header)
+                if 'captcha.search.daum.net' in req.text:
+                    print(req.text)
+                else:
+                    soup = BeautifulSoup(req.text, 'html.parser')
+                    articles = soup.select('.tit-g')
+                    # if 'naver' in url:
+                    #     articles = soup.select('.news_tit')
+                    # elif 'daum' in url:
+                    #     articles = soup.select('.tit_main')
+                    for article in articles:
+                        article_find = article.find('a')
+                        title = article_find.text
+                        link = article_find['href']
+                        if title not in self.list_set:
+                            self.list_set.add(title)
+                            print(title, link)
+                        else:
+                            break
+                time.sleep(10)
 
         except KeyboardInterrupt:
             print('\nfinish')
             pass
 
-    def crawl(self, urls):
-        pool = ThreadPoolExecutor()
+    def crawl(self):
+        pool = ThreadPoolExecutor(max_workers = 32)
         for url in self.urls:
             pool.submit(self.run, url)
     
@@ -59,7 +64,7 @@ class Crawl():
 
 def main():
     c = Crawl()
-    c.crawl(c.urls)
-
+    c.crawl()
+    
 if __name__ == '__main__':
     main()
